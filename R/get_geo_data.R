dfee0c4ef3d8b3a181525735338f903b7f8fa13a
Chanyub Park <mrchypark@gmail.com>
2018-12-16 16:22:13 +0900

    Initial commit

---
+++ R/get_geo_data.R
@@ -0,0 +1,6 @@
+#' get Geographic Data
+#'
+#' @importFrom crayon hyperlink
+get_geo_data <- function(){
+  cat("Please read ", crayon::hyperlink("this link", "http://kgss.skku.edu/?page_id=1213"), ".\n")
+}
